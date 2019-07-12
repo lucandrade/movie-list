0e80aa2602463c06acf6d7a24090e9de45d5b73e
Lucas Andrade <lucasandradedeveloper@gmail.com>
2019-07-12 19:24:31 +0100

    adding search functionality

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
-export default class NavBar extends Component {
+class NavBar extends Component {
+    constructor(props) {
+        super(props);
+        this.input = React.createRef();
+    }
+
+    componentDidMount() {
+        const params = new URLSearchParams(this.props.location.search);
+        const query = params.get('q');
+
+        if (!query) {
+            return;
+        }
+
+        this.input.current.value = decodeURI(query);
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const path = `/search?q=${this.input.current.value}`;
+        this.props.history.push(encodeURI(path));
+    }
+
     render() {
         return (
-            <nav className="w-full fixed top-0 left-0 p-6 flex bg-black text-white">
+            <nav className="w-full fixed top-0 left-0 p-6 flex items-center bg-black text-white">
                 <div className="">
                     <Link to="/">MovieList</Link>
                 </div>
+                <form className="flex-1 pl-6" onSubmit={this.onSubmit.bind(this)}>
+                    <input type="text"
+                            ref={this.input}
+                            className="shadow appearance-none border rounded-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            placeholder="Search" />
+                </form>
             </nav>
         );
     }
 }
+
+export default withRouter(NavBar);
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,7 +5,7 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                <h1>Popular Movies</h1>
+                <h1 className="text-3xl font-semibold mb-6">Popular Movies</h1>
                 <Link to="/search" className="bg-blue-600 px-4 py-2 rounded inline-block">Search</Link>
             </div>
         );
