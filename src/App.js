426fc3cd4d815a2c064fb97a09318c3bbab49aea
Lucas Andrade <lucasandradedeveloper@gmail.com>
2019-07-12 19:01:22 +0100

    adding navbar and basic style

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
+import NavBar from './Components/NavBar';
 
-function App() {
-    return (
-        <div className="p-6">
-            <header>
-                <p>Edit <code>src/App.js</code> and save to reload.</p>
-                <a
-                    className="bg-blue-700 text-white px-6 py-2 rounded-full inline-block"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+export default class App extends Component {
+    render() {
+        return (
+            <div className="h-screen bg-gray-800 text-white p-6 pt-24">
+                <NavBar />
+                <h1 className="m-0 p-0">Popular Movies</h1>
+            </div>
+        );
+    }
 }
-
-export default App;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class NavBar extends Component {
+    render() {
+        return (
+            <nav className="w-full fixed top-0 left-0 p-6 flex bg-black text-white">
+                <div className="">
+                    MovieList
+                </div>
+            </nav>
+        );
+    }
+}
