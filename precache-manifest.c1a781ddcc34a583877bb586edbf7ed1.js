1cd7132ed5d78229e6e858e1891466d23e9dc362
Lucas Andrade de Oliveira <lucasandradedeveloper@gmail.com>
2019-07-17 23:07:20 +0100

    Updates

---
+++ precache-manifest.c1a781ddcc34a583877bb586edbf7ed1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85f36e2ecf86f06e9b2add6f01031fe7",
+    "url": "/movie-list/index.html"
+  },
+  {
+    "revision": "b8e3abbb624aa9d95a4e",
+    "url": "/movie-list/static/css/main.d8a05234.chunk.css"
+  },
+  {
+    "revision": "eb235175caa4dc39d8cd",
+    "url": "/movie-list/static/js/2.72e3c039.chunk.js"
+  },
+  {
+    "revision": "b8e3abbb624aa9d95a4e",
+    "url": "/movie-list/static/js/main.3b1c2a77.chunk.js"
+  },
+  {
+    "revision": "f36c80dcc518bdc2b711",
+    "url": "/movie-list/static/js/runtime~main.de49e91d.js"
+  }
+]);
