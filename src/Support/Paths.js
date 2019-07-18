c373273ddae42c7fb1de87a2817a354a7f41448c
Lucas Andrade de Oliveira <lucasandradedeveloper@gmail.com>
2019-07-18 01:48:37 +0100

    hash router

--- src/Support/Paths.js
+++ src/Support/Paths.js
@@ -1,5 +1,5 @@
 const isProduction = process.env.NODE_ENV === 'production';
-const prefix = '/' + (isProduction ? 'movie-list/' : '');
+const prefix = '/' + (isProduction && false ? 'movie-list/' : '');
 
 export const PATH_HOME = prefix;
 export const PATH_LOGS = `${prefix}logs`;
